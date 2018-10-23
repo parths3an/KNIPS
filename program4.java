6ea37ca8b4f8d224fb8767863cfcaab9765d5b93
parths3an <parth.93mukesh@gmail.com>
2018-10-23 11:21:56 -0700

    Working on program 4

---
+++ program4.java
@@ -0,0 +1,19 @@
+//FLoating point number to an Integer 
+import java.util.*;
+import java.lang.*;
+
+public class program4
+{
+
+    //r0 = I[7:0], r1 = I[15:8]
+    public static void main(String[] args) {
+        short I = 32000; 
+        byte r0;
+        byte r1;
+        System.out.println(Integer.toBinaryString(0xFFFF & I));
+        
+}
+
+
+
+}
