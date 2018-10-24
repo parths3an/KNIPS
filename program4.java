cdfe42694da738b8610dc75886cc4537f1b8c652
parths3an <parth.93mukesh@gmail.com>
2018-10-23 18:08:58 -0700

    working on 4

--- program4.java
+++ program4.java
@@ -10,26 +10,63 @@ public class program4
         short I = 32750; 
         short r0 = I;               //#1
         byte r1; 
-        byte E;
-        short r3;
-//      System.out.println(Integer.toBinaryString(0xFFFF & I));
+        int E;
+        byte r31;
+        byte r32;
+      System.out.println(Integer.toBinaryString(0xFFFF & I));
 
     byte s = (byte) 0;              
-    r1 = s;                         //#2
-    E = (byte) 29;                  //#3
-    r3 = I;                         //#4
-    System.out.println("Before entering the loop r3 is: " + Integer.toBinaryString(0xFFFF & r3));
+    r1 = s;                                            //#2
+    E =  29;                                           //#3
+    r31 = (byte) (I & 0xFFFF);      //I[7:0]           //#4
+    r32 = (byte) ( I >> 8 & 0xFFFF); // I[14:8]
+    System.out.println("Before entering the loop r31 is: " + Integer.toBinaryString(0xFF & r31));
+    System.out.println("Before entering the loop r32 is: " + Integer.toBinaryString(0xFF & r32));
 
-    //#4
-    for(int i =0;  i <15; i++)
+    byte r14 = (byte) 1 << 6;
+    byte r4 = (byte) 1 << 4;
+    byte r3 = (byte) 1 << 3; 
+    //System.out.println("r14 is: " + Integer.toBinaryString(0xFFFF & r14));
+    //System.out.println("r4 is: " + Integer.toBinaryString(0xFFFF & r4));
+    //System.out.println("r3 is: " + Integer.toBinaryString(0xFF & r3));
+    
+    //#5
+    for (int i=0; i < 7; i++)
     {
-        byte msb = (byte) ((r3 << 13)& 0xffff);
-        if ( msb == 0)
+        if ( ((byte)(r32 & r14) >> 6) == 0)
         {
-            System.out.println(Integer.toBinaryString(msb));
+            r31 = (byte) (r31 << 1);
+            r32 = (byte) (r32 << 1);
+            E--; 
+            System.out.print("updated r31: " +Integer.toBinaryString(0xFF & r31));
+            System.out.print("updated r32: " +Integer.toBinaryString(0xFF & r32));
+            
         }
-        System.out.println("The msb is: " + Integer.toBinaryString(msb));
-        System.out.print("After finding msb r3 is: " +  Integer.toBinaryString(0xFFFF & r3));
-    }
+        else{
+           break; 
+        }
+}
+
+byte checkR3 = (byte)((r31 & r3) >> 3);
+byte checkR4 = (byte)((r31 & r4) >> 4);
+byte checkFirst3 = (byte) ((r31 << 5)) ; 
+byte first3 = (byte) (0x00 &  0x00);
+
+System.out.print("Check first3 : " +Integer.toBinaryString(0xFF & checkFirst3));
+System.out.print("Check first3 : " +Integer.toBinaryString(0xFF & first3));
+
+//#6.
+if (checkR3 == 1 && checkR4 == 1)
+{
+    //How to do this for two seprate bits 
+    //add r3 +8
+
+}
+
+if (checkR3 == 1 && checkR4 == 0 )
+{
+
+}
+
 }
 }
